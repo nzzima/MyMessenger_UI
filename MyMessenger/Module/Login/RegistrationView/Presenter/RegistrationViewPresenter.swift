a1c124331507c56bb4e43ae119fd260fabece89a
nzzima <nikita-krylov-2012@mail.ru>
2024-12-16 17:19:59 +0300

    Starting registration window. Stopped on fields...

---
+++ MyMessenger/Module/Login/RegistrationView/Presenter/RegistrationViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewPresenter.swift
+//  MyMessenger
+//
+//  Created by Nikita Krylov on 16.12.2024.
+//
+
+import Foundation
---
+++ MyMessenger/Module/Login/RegistrationView/View/RegistrationView.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationView.swift
+//  MyMessenger
+//
+//  Created by Nikita Krylov on 16.12.2024.
+//
+
+import Foundation
